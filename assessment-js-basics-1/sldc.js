78ca2c4817df52ff8118935be1417a4c3d1699e8
William Cuthbertson <whc1315@gmail.com>
2022-01-07 08:57:14 -0700

    SLDC done.

--- assessment-js-basics-1/sldc.js
+++ assessment-js-basics-1/sldc.js
@@ -5,12 +5,12 @@
     Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
 */
 
+// Planning
 // Analysis of Requirements
 // Design
 // Implementation
-// Maintain
-// Planning
 // Testing & Integration
+// Maintain
 
 
 //////////////////PROBLEM 2////////////////////
@@ -19,3 +19,8 @@
     The first one is done for you. 
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const analysisOfRequirements = "This is the second step, defining objectives, project goals and what operations and functions the application will perform."
+const design = "This is the third step, detailed screen layout, rules, process diagrams, psuedocode and other documetation set out. Beginning of development, secure code begins to be written."
+const implementation = "This is the fourth step, this phase focuses on testing the code that has been written for bugs and other inconsistencies. Testing and quality assurance."
+const testingAndIntegration = "This is the fifth step, this is the deployment or production phase. This comes after it has been developed, tested, fixed, retested and validated."
+const maintenance = "This is the sixth and final step, there is corrective and perfective maintenance. Users will suggest new features and find things that could be tweaked, this will send the project back for corrective maintenance. Meanwhile, the team working on this project will be able to evaluate the new requirement the projects faces on a daily basis and be able to put out updates on their own, this being the perfective maintenance."
