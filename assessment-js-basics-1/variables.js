766dcd2dad43501e2f21a864868c07d3642b8bce
William Cuthbertson <whc1315@gmail.com>
2022-01-07 09:03:24 -0700

    Still chugging

--- assessment-js-basics-1/variables.js
+++ assessment-js-basics-1/variables.js
@@ -3,16 +3,21 @@
 
 //CODE HERE 
 
+let myName = 'William'
+//console.log(myName)
 //////////////////PROBLEM 2////////////////////
 /* Create a variable called 'faveNum' with a value that is your favorite number. */
 
 //CODE HERE
 
+let faveNum = 0
+//console.log(faveNum)
 //////////////////PROBLEM 3////////////////////
 /* Create a variable called 'lovesCode' and assign it a boolean value. */
 
 //CODE HERE
 
+let lovesCode = true
 //////////////////PROBLEM 4////////////////////
 /*
   Write an if-statement that checks if the faveNum variable is 
@@ -22,6 +27,11 @@
 
 //CODE HERE
 
+if (faveNum === 13){
+  console.log('lucky 13!')
+} else {
+  console.log('not lucky 13')
+}
 //////////////////PROBLEM 5////////////////////
 /*
   Write a for loop that prints out your faveNum 5 times.
